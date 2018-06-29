112c7dfd96a1b745c615b267e60df613a2cb7c84
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-29 22:58:42 +0100

    creates booking class and tests set up

---
+++ src/main/java/Booking.java
@@ -0,0 +1,33 @@
+public class Booking {
+
+    private Hotel hotel;
+    private Bedroom bedroom;
+    private Guest guest;
+    private int nights;
+
+    public Booking(Hotel hotel, Bedroom bedroom, Guest guest, int nights) {
+        this.hotel = hotel;
+        this.bedroom = bedroom;
+        this.guest = guest;
+        this.nights = nights;
+    }
+
+    public Hotel getHotel() {
+        return this.hotel;
+    }
+
+    public Bedroom getBedroom() {
+        return this.bedroom;
+    }
+
+    public Guest getGuest() {
+        return this.guest;
+    }
+
+    public int getNights() {
+        return this.nights;
+    }
+
+
+
+}
